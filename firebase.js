acb20aaeaa21bb09464f16fa9406398c6cacbb5c
Adryan <adryanpatrick77@gmail.com>
2026-06-26 08:39:35 -0300

    0

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } 
 from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-
 import { getMessaging, getToken }
 from "https://www.gstatic.com/firebasejs/12.15.0/firebase-messaging.js";
 
@@ -25,72 +24,43 @@ appId: "1:637444451377:web:059a3640c800a290e0b910"
 
 const app = initializeApp(firebaseConfig);
 
-
 const messaging = getMessaging(app);
 
 
-
 async function ativarNotificacao(){
-
 
   if(!("Notification" in window)){
     console.log("Navegador sem suporte");
     return;
   }
 
-
-  const permissao =
-  await Notification.requestPermission();
-
-
+  const permissao = await Notification.requestPermission();
 
   if(permissao === "granted"){
 
-
     console.log("Permissão liberada ❤️");
 
-
-
-    const registroSW =
-    await navigator.serviceWorker.register(
-      "./firebase-messaging-sw.js"
+    /* ✅ CORREÇÃO IMPORTANTE: SW na raiz */
+    const registroSW = await navigator.serviceWorker.register(
+      "/firebase-messaging-sw.js"
     );
 
+    const token = await getToken(messaging, {
 
+      vapidKey:
+      "BISBY2DVRPxvlbQSLjVvUPFUY1NRJWLL844d2f9HifgOI8J7o3y7QD3PJrNupxbW4jG23i0LRS0ceq_86aVADUw",
 
-    const token =
-    await getToken(
-      messaging,
-      {
+      serviceWorkerRegistration: registroSW
 
-        vapidKey:
-        "BISBY2DVRPxvlbQSLjVvUPFUY1NRJWLL844d2f9HifgOI8J7o3y7QD3PJrNupxbW4jG23i0LRS0ceq_86aVADUw",
+    });
 
+    console.log("TOKEN DO CELULAR:", token);
 
-        serviceWorkerRegistration:
-        registroSW
-
-      }
-    );
-
-
-
-    console.log(
-      "TOKEN DO CELULAR:",
-      token
-    );
-
-
-  }else{
-
-    console.log(
-      "Notificação bloqueada"
-    );
-
+  } else {
+    console.log("Notificação bloqueada");
   }
 
 }
-
 
 
 ativarNotificacao();
