349a6be734bc9c2dbfb47bd1f3234acc787a8f0d
Adryan <adryanpatrick77@gmail.com>
2026-06-26 09:28:31 -0300

    5

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,55 +1,31 @@
-importScripts(
-"https://www.gstatic.com/firebasejs/12.15.0/firebase-app-compat.js"
-);
-
-importScripts(
-"https://www.gstatic.com/firebasejs/12.15.0/firebase-messaging-compat.js"
-);
-
-
+importScripts("https://www.gstatic.com/firebasejs/12.15.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.15.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-
-apiKey: "AIzaSyCe-MBs8Kusu-HCV3MpqiIZjolzHmxKf5s",
-
-authDomain: "nosso-cuidado.firebaseapp.com",
-
-projectId: "nosso-cuidado",
-
-storageBucket: "nosso-cuidado.firebasestorage.app",
-
-messagingSenderId:"637444451377",
-
-appId:"1:637444451377:web:059a3640c800a290e0b910"
-
+  apiKey: "AIza...",
+  authDomain: "nosso-cuidado.firebaseapp.com",
+  projectId: "nosso-cuidado",
+  storageBucket: "nosso-cuidado.firebasestorage.app",
+  messagingSenderId: "637444451377",
+  appId: "1:637444451377:web:059a3640c800a290e0b910"
 });
 
+const messaging = firebase.messaging();
 
+messaging.onBackgroundMessage((payload) => {
 
-const messaging =
-firebase.messaging();
+  const title =
+    payload.notification?.title ||
+    payload.data?.title ||
+    "Notificação";
 
+  const body =
+    payload.notification?.body ||
+    payload.data?.body ||
+    "";
 
-
-messaging.onBackgroundMessage(
-(payload)=>{
-
-
-self.registration.showNotification(
-
-payload.notification.title,
-
-{
-
-body:
-payload.notification.body,
-
-icon:
-"./projeto-jay-remedio/icone192.png"
-
-}
-
-);
-
-
+  self.registration.showNotification(title, {
+    body: body,
+    icon: "/projeto-jay-remedio/icone192.png"
+  });
 });
